d920eb711604f51788250081906e3739f43edcd7
Hussein Mohamad Hallak <hu100wtf@gmail.com>
2019-09-21 21:37:16 -0400

    adds profile update

--- .eslintrc.js
+++ .eslintrc.js
@@ -43,6 +43,7 @@ module.exports = {
     "react-hooks/rules-of-hooks": "error",
     "react-hooks/exhaustive-deps": "warn",
     "react/jsx-props-no-spreading": "off",
+    "prefer-object-spread" : "off"
   },
   settings: {
     "import/resolver": {
