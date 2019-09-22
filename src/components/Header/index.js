d0928bed6acf3b9aa8334325b92b87e91f6ee5be
Hussein Mohamad Hallak <hu100wtf@gmail.com>
2019-09-21 22:27:05 -0400

    fix: null avatar on header

--- src/components/Header/index.js
+++ src/components/Header/index.js
@@ -25,7 +25,7 @@ export default function Header() {
             </div>
             <img
               src={
-                profile.avatar.url ||
+                profile.avatar ||
                 'https://api.adorable.io/avatars/50/abott@adorable.png'
               }
               alt="Diego Fernandes"
